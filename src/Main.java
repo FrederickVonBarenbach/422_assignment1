bfdc6db7e0d7049140a159afef0d8d36ac5596bf
Frederick Shpilevskiy <f.shpilevskiy@gmail.com>
2022-09-26 21:43:49 -0700

    Final commit
    - improved readability (hopefully)

--- src/Main.java
+++ src/Main.java
@@ -3,228 +3,219 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    static boolean passableMap[][] = { {true, true, true},
-                                       {true, false, true},
-                                       {true, true, true},
-                                       {true, true, true} };
-    static double rewardMap[][] = { {0, 0, 0},
-                                    {0, 0, 0},
-                                    {0, 0, 0},
-                                    {0, -1, 1} };
-//    float beliefState[][] = new float[3][4];
-//    for (int i=0; i<3; i++){
-//
-//    }
-                                // U    D    R    L (actual action)
-    static double actions[][] = { {0.8, 0  , 0.1, 0.1},   // U (desired action)
-                                  {0  , 0.8, 0.1, 0.1},   // D
-                                  {0.1, 0.1, 0.8, 0  },   // R
-                                  {0.1, 0.1, 0  , 0.8} }; // L
+    // Problem specifications
 
-                                    // 1    2    end
-    static double observables[][] = { {0.9, 0.1, 0  }, // 1 wall
-                                      {0.1, 0.9, 0  }, // 2 wall
-                                      {0  , 0  , 1  }};// terminal
+    // For our maze problem, we have a passability map that indicates whether some state
+    // is a wall (a wall is false, a non-wall is true)
+    static Boolean[][] passableMap = { {true, true, true, true},
+                                       {true, false, true, true},
+                                       {true, true, true, true} };
 
-//    static double observables[][] = { { 1, 1, 1 }, // 1 wall
-//                                      {1, 1, 1 }, // 2 wall
-//                                      {1, 1, 1 }};// terminal
+    // The reward map must have the same size as the passability map and sets a reward at
+    // the corresponding state
+    static Double[][] rewardMap = { {0.0, 0.0, 0.0, 1.0},
+                                    {0.0, 0.0, 0.0, -1.0},
+                                    {0.0, 0.0, 0.0, 0.0} };
 
-    private static class State {
-        int i;
-        int j;
-        int id;
+    // This is a set of actions where the action our agent intends to take is the row and
+    // the action our agent actually takes is the column (e.g. take action UP but the agent
+    // actually goes RIGHT)
+                                // UP    DOWN  RIGHT LEFT (actual action)
+    static double actions[][] = { {0.8,  0  ,  0.1,  0.1},   // UP (taken action)
+                                  {0  ,  0.8,  0.1,  0.1},   // DOWN
+                                  {0.1,  0.1,  0.8,  0  },   // RIGHT
+                                  {0.1,  0.1,  0  ,  0.8} }; // LEFT
+
+    // This is a set of observations where the true value is the row and the observed value is the column
+                                    // 1    2    end observation
+    static double observables[][] = { {0.9, 0.1, 0  }, // 1 wall states
+                                      {0.1, 0.9, 0  }, // 2 wall states
+                                      {0  , 0  , 1  }};// terminal states
+
+
+    static int width = passableMap[0].length;
+    static int height = passableMap.length;
+
+    public static void main(String[] args) {
+        // Initialize the states according to the specifications above
+        State[][] stateMap = new State[width][height];
+        List<State> stateList = new ArrayList<>();
+        initializeStates(stateMap, stateList);
+
+        // Specify initial belief state as a map
+        Double[][] beliefStateMap = { {0.0, 0.0, 0.0, 0.0},
+                                      {0.0, 0.0, 0.0, 0.0},
+                                      {1.0, 0.0, 0.0, 0.0} };
+
+        // Specify actions as a list of integers
+        // An action is an integer where 0 is UP, 1 is DOWN, 2 is RIGHT, 3 is LEFT
+        int[] actions = {0, 2, 2, 2};
+
+        // Specify observations as a list of integers
+        // An observation is an integer where 0 is 1 WALL, 1 is 2 WALL, and 3 is TERMINAL
+        int[] observations = {1, 1, 0, 0};
+
+        computeBeliefState(beliefStateMap, actions, observations, stateMap, stateList);
+    }
+
+
+    static class State {
+        int i; // row
+        int j; // column
+        int id; // index of stateList
         double reward;
 
         State(int i, int j, int id) {
             this.i = i;
             this.j = j;
             this.id = id;
-            neighbours = new State[4];
-            transition = new double[11][4];
-            observable = new double[3];
+            neighbours = new State[actions.length];
+            transition = new double[width*height][actions.length];
+            observable = new double[observables.length];
         }
 
-        public void printState(List<State> stateList) {
-            System.out.println("==========================");
-            System.out.println("My value: " + i + ", " + j);
-            System.out.println("\tNeighbours:");
-            for (State state : neighbours) {
-                if (state != null) {
-                    System.out.println("\t\t" + state.i + ", " + state.j);
-                } else {
-                    System.out.println("\t\tWall");
-                }
-            }
-            System.out.println("\tTransition:");
-            System.out.println("\t\t\tU   D   R   L");
-            for (int i = 0; i < transition.length; i++) {
-                State state = stateList.get(i);
-                String output = "\t(" + state.i + ", " + state.j + ")";
-                for (double probability : transition[i]) {
-                    output += " " + probability;
-                }
-                System.out.println(output);
-            }
-//            System.out.println("\tObservable:");
-//            System.out.println("\t 1   2  end");
-//            String output = "\t";
-//            for (double probability : observable) {
-//                output += probability + " ";
-//            }
-//            System.out.println(output);
-            System.out.println("==========================");
-        }
-
-        State[] neighbours;
-        double[][] transition;
-        double[] observable;
+        State[] neighbours; // Neighbours of the state with respect to the action taken
+                            // (e.g. neighbour at index 0 is the state above this one)
+        double[][] transition; // Such that transition[S'][A] = P(S' | this state, A)
+        double[] observable;   // Such that observable[e] = P(e | this state)
     }
 
+    static <T> T get(T[][] map, int i, int j) {
+        return map[map.length-1-j][i];
+    }
 
-    public static void main(String[] args) {
-        State[][] stateMap = new State[4][3];
-        List<State> stateList = new ArrayList<>();
+    static void initializeStates(State[][] stateMap, List<State> stateList) {
 
         // Initializing states
-        for (int i = 0; i < passableMap.length; i++)  {
-            for (int j = 0; j < passableMap[i].length; j++) {
-                // Passable map is inverted
-                if (passableMap[i][j]) {
+        for (int i = 0; i < width; i++)  {
+            for (int j = 0; j < height; j++) {
+                // Passable map is converted into a state board/matrix, also have a copy
+                // represented as a list of states
+                if (get(passableMap, i, j)) {
                     State state = new State(i, j, stateList.size());
                     stateList.add(state);
                     stateMap[i][j] = state;
                     // Set up rewards
-                    state.reward = rewardMap[i][j];
+                    state.reward = get(rewardMap, i, j);
                 }
             }
         }
 
-        // Setting up adjacencies
-        for (int i = 0; i < stateMap.length; i++) {
-            for (int j = 0; j < stateMap[i].length; j++) {
+        // Setting up adjacencies/neighbours
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 State state = stateMap[i][j];
                 if (state != null) {
-                    if (state.j+1 < passableMap[i].length) state.neighbours[0] = stateMap[state.i][state.j+1]; // UP
+                    // Get the neighbours of a state according to the actions (e.g. 0 is UP)
+                    if (state.j+1 < height) state.neighbours[0] = stateMap[state.i][state.j+1]; // UP
                     if (state.j-1 >= 0) state.neighbours[1] = stateMap[state.i][state.j-1]; // DOWN
-                    if (state.i+1 < passableMap.length) state.neighbours[2] = stateMap[state.i+1][state.j]; // RIGHT
+                    if (state.i+1 < width) state.neighbours[2] = stateMap[state.i+1][state.j]; // RIGHT
                     if (state.i-1 >= 0) state.neighbours[3] = stateMap[state.i-1][state.j]; // LEFT
                 }
             }
         }
 
-        // Setting up probabilities
+        // Setting up transition model
         for (State state : stateList) {
             // Transition
-            if (state.reward != 0) { // Terminal
+            if (state.reward != 0) { // If this is a terminal state
                 for (int action = 0; action < actions.length; action++) {
-                    state.transition[state.id][action] = 1;
+                    state.transition[state.id][action] = 1; // No action can move the agent from the terminal state
                 }
             } else { // Non-terminal
+                // Determine P(S' | state, A)
                 for (int actionThatOccurs = 0; actionThatOccurs < actions.length; actionThatOccurs++) {
                     for (int actionTaken = 0; actionTaken < actions[actionThatOccurs].length; actionTaken++) {
                         State neighbour = state.neighbours[actionThatOccurs];
                         if (neighbour != null) {
-                            // No wall means update neighbour
+                            // No wall means add probability to neighbour states
                             state.transition[neighbour.id][actionTaken] += actions[actionThatOccurs][actionTaken];
                         } else {
-                            // If hit a wall, add to our own probability
+                            // If hit a wall, add probability to this state itself
                             state.transition[state.id][actionTaken] += actions[actionThatOccurs][actionTaken];
                         }
                     }
                 }
             }
 
-            // Observable
-            if (state.reward != 0) { // Terminal
+            // Setting up observation model
+            if (state.reward != 0) { // If terminal state, set P(e | state) to terminal observable
                 state.observable = observables[2].clone();
             } else {
                 int numWalls = 0;
                 for (State neighbour : state.neighbours) {
                     if (neighbour == null) numWalls++;
                 }
-                if (numWalls == 1) { // 1 wall
+                if (numWalls == 1) { // If 1 wall, set P(e | state) to 1 wall observable
                     state.observable = observables[0].clone();
-                } else if (numWalls == 2) { // 2 wall
+                } else if (numWalls == 2) { // If 2 wall, set P(e | state) to 2 wall observable
                     state.observable = observables[1].clone();
                 } else {
                     throw new RuntimeException("Impossible");
                 }
             }
         }
-
-//        for (State state : stateList) {
-//            state.printState(stateList);
-//        }
-
-//        double[][] beliefStateMap = { {0.1111, 0.1111, 0.1111},
-//                                      {0.1111, 0,      0.1111},
-//                                      {0.1111, 0.1111, 0.1111},
-//                                      {0.1111, 0,      0} };
-        double[][] beliefStateMap = { {1, 0, 0},
-                                      {0, 0, 0},
-                                      {0, 0, 0},
-                                      {0, 0, 0} };
-        int[] actions = {0, 2, 2, 2};
-        int[] observations = {1, 1, 0, 0};
-        computeBeliefState(beliefStateMap, actions, observations, stateMap, stateList);
     }
 
-    // An action is an integer where 0 is UP, 1 is DOWN, 2 is RIGHT, 3 is LEFT
-    // An observation is an integer where 0 is 1 WALL, 1 is 2 WALL, and 3 is TERMINAL
-    public static void computeBeliefState(double[][] beliefStateMap, int[] actions, int[] observations,
+    static void computeBeliefState(Double[][] beliefStateMap, int[] actions, int[] observations,
                                           State[][] stateMap, List<State> stateList) {
-        // Build beliefStateList
+        // Build beliefStateList from beliefStateMap
         List<Double> beliefStateList = Arrays.asList(new Double[stateList.size()]);
-        for (int i = 0; i < beliefStateMap.length; i++) {
-            for (int j = 0; j < beliefStateMap[i].length; j++) {
-                if (stateMap[i][j] != null) beliefStateList.set(stateMap[i][j].id, beliefStateMap[i][j]);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (stateMap[i][j] != null) beliefStateList.set(stateMap[i][j].id, get(beliefStateMap, i, j));
             }
         }
 
         // Update belief
         for (int iteration = 0; iteration < actions.length; iteration++) {
-            List<Double> beliefStatePrime = new ArrayList<>(beliefStateList);
-            double normalizationFactor = 0;
-            int a = actions[iteration];
-            int e = observations[iteration];
+            List<Double> beliefStateListPrime = new ArrayList<>(beliefStateList); // This is b'
+            double normalizationFactor = 0; // This is 1/alpha
+            int a = actions[iteration]; // This is a
+            int e = observations[iteration]; // This is e
 
             for (int sPrime = 0; sPrime < stateList.size(); sPrime++) {
-                double sum = 0;
+                double sum = 0; // sum_s P(s' | a, s)b(s)
                 for (int s = 0; s < stateList.size(); s++) {
                     sum += stateList.get(s).transition[sPrime][a] * beliefStateList.get(s);
                 }
+                // b'(s') = P(e | s') * sum
                 double bOfSPrime = stateList.get(sPrime).observable[e] * sum;
                 normalizationFactor += bOfSPrime;
-                State statePrime = stateList.get(sPrime);
-//                System.out.println("Iteration: " + iteration + " | ( " + statePrime.i + ", " + statePrime.j + ") = "
-//                                    + bOfSPrime);
-                beliefStatePrime.set(sPrime, bOfSPrime);
+                beliefStateListPrime.set(sPrime, bOfSPrime);
             }
-            // Normalize
-            for (int i = 0; i < beliefStatePrime.size(); i++) {
-                double unNormalized = beliefStatePrime.get(i);
-                beliefStatePrime.set(i, unNormalized/normalizationFactor);
+            // Normalize using alpha (normalizationFactor)
+            for (int i = 0; i < beliefStateListPrime.size(); i++) {
+                double unNormalized = beliefStateListPrime.get(i);
+                // b'(s') * normalizationFactor
+                beliefStateListPrime.set(i, unNormalized/normalizationFactor);
             }
 
-            beliefStateList = beliefStatePrime;
+            // Repeat process for next action, observation pair
+            beliefStateList = beliefStateListPrime;
         }
 
         // Build beliefStateMap
+        beliefStateMap = new Double[width][height];
         for (int i = 0; i < beliefStateList.size(); i++) {
             State state = stateList.get(i);
             beliefStateMap[state.i][state.j] = beliefStateList.get(i);
         }
 
+        // Visualize beliefState
         printBeliefState(beliefStateMap);
     }
 
-    public static void printBeliefState(double[][] beliefStateMap) {
+    static void printBeliefState(Double[][] beliefStateMap) {
         System.out.println("");
-        for (int j = beliefStateMap[0].length-1; j >= 0; j--) {
+        for (int j = height-1; j >= 0; j--) {
             String row = "";
-            for (int i = 0; i < beliefStateMap.length; i++) {
-                row += String.format("%.3f", beliefStateMap[i][j]) + "\t\t";
+            for (int i = 0; i < width; i++) {
+                if (beliefStateMap[i][j] == null) {
+                    row += "WALL \t\t";
+                } else {
+                    row += String.format("%.3f", beliefStateMap[i][j]) + "\t\t";
+                }
             }
             System.out.println(row + "\n");
         }
